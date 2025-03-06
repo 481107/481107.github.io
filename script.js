eaeb3f451f67b6d937f05023df915883a7a66dd6
481107 <31226731+481107@users.noreply.github.com>
2025-03-06 07:18:49 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const determineGreet = hours => document.getElementById("greeting").innerText = 
 
 window.addEventListener('load', (event) => {
     let today = new Date();
-    let date = today.toLocaleDateString([], { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' });
-    let time = today.toLocaleTimeString([], { hour12: false, hour: '2-digit', minute: '2-digit' });
+    let date = today.toLocaleDateString("en-CA", { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' });
+    let time = today.toLocaleTimeString("en-CA", { hour12: false, hour: '2-digit', minute: '2-digit' });
     determineGreet(new Date().getHours());
     displayDate(date);
     displayTime(time);
@@ -11,8 +11,8 @@ window.addEventListener('load', (event) => {
 
 setInterval(function () {
     var today = new Date();
-    let date = today.toLocaleDateString([], { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' });
-    var time = today.toLocaleTimeString([], { hour12: false, hour: '2-digit', minute: '2-digit' });
+    let date = today.toLocaleDateString("en-CA", { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' });
+    var time = today.toLocaleTimeString("en-CA", { hour12: false, hour: '2-digit', minute: '2-digit' });
     document.getElementById("date").innerHTML = date;
     document.getElementById("time").innerHTML = time;
 }, 1000);
