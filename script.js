493fa2c504422cea3752eed5f0c3a92d5fc6e9a1
481107 <31226731+481107@users.noreply.github.com>
2025-03-06 06:24:38 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,25 @@ const determineGreet = hours => document.getElementById("greeting").innerText = 
 
 window.addEventListener('load', (event) => {
     let today = new Date();
+    let date = today.toLocaleDateString("en-US", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
     let time = today.toLocaleTimeString([], { hour12: false, hour: '2-digit', minute: '2-digit' });
     determineGreet(new Date().getHours());
+    displayDate(date);
     displayTime(time);
 });
 
 setInterval(function () {
     var today = new Date();
+    let date = today.toLocaleDateString("en-US", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
     var time = today.toLocaleTimeString([], { hour12: false, hour: '2-digit', minute: '2-digit' });
+    document.getElementById("date").innerHTML = date;
     document.getElementById("time").innerHTML = time;
 }, 1000);
 
 function displayTime(time) {
     document.getElementById("time").innerHTML = time;
+}
+
+function displayDate(date) {
+    document.getElementById("date").innerHTML = date;
 }
